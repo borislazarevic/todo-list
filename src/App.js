e56dc29d69783d677b0b1f7f4775947b03b077ba
borislazarevic <boris1991@live.com>
2021-06-01 15:36:51 +0200

    default setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './App.module.css';
+
+function App() {
+  return (
+    <div className={classes.App}>
+      <h1>Todo List</h1>
+    </div>
+  );
+}
+
+export default App;
