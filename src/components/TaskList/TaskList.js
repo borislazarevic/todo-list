f69d10da752ff2e9fc7fb590f6d21092c60b88bf
borislazarevic <boris1991@live.com>
2021-06-07 11:18:58 +0200

    scrollbar improved

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import classes from './TaskList.module.css';
 import Task from './Task/Task';
@@ -6,18 +7,28 @@ import Task from './Task/Task';
 const TaskList = ({ tasks, setTasks }) => {
   return (
     <div className={classes.TaskListContainer}>
-      <ul className={classes.TaskList}>
-        {tasks.map(task => (
-          <Task
-            key={task.id}
-            text={task.text}
-            id={task.id}
-            tasks={tasks}
-            setTasks={setTasks}
-            task={task}
-          />
-        ))}
-      </ul>
+      <Scrollbars
+        autoHide
+        autoHideTimeout={500}
+        autoHideDuration={200}
+        style={{
+          height: 420,
+          width: 700,
+        }}
+      >
+        <ul className={classes.TaskList}>
+          {tasks.map(task => (
+            <Task
+              key={task.id}
+              text={task.text}
+              id={task.id}
+              tasks={tasks}
+              setTasks={setTasks}
+              task={task}
+            />
+          ))}
+        </ul>
+      </Scrollbars>
     </div>
   );
 };
